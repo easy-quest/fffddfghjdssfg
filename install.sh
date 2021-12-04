843dcbaf542ee6ce7c0a92790ad24af206d14503
Ivan Yastrebov <easy-quest@mail.ru>
2021-12-04 16:14:45 +0300

    Android Sat Dec  4 16:14:45 MSK 2021

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#=============================================================================
+# basic.bash --- basic script files
+# Copyright (c) 2016-2021 nvspc & EasyQuest
+# Author: Ivan Yastrebov < easy-quest@mail.ru >
+# URL: https://easyquest.host
+#=============================================================================
+
+set -e # -e: exit on error
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
---
+++ my_abbr.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/env fish
+#=============================================================================
+# basic.bash --- basic script files
+# Copyright (c) 2016-2021 nvspc & EasyQuest
+# Author: Ivan Yastrebov < easy-quest@mail.ru >
+# URL: https://easyquest.host
+#=============================================================================
+
+
+abbr -a -U -- fix 'termux-fix-shebang '
+abbr -a -U -- m 'micro '
+abbr -a -U -- xclip 'termux-clipboard-set '
+abbr -a -U -- xx 'chmod +x '
+abbr -a -U -- z 'chezmoi '
+abbr -a -U -- ze 'chezmoi edit --apply $argv'
